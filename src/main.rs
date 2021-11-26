8189e3214450ad9dc51b419608aeaf51112417b8
Jan Niehusmann <jan@gondor.com>
2021-11-26 18:27:48 +0000

    test case

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,6 @@ fn main() -> ! {
     let mut pac = pac::Peripherals::take().unwrap();
     let core = pac::CorePeripherals::take().unwrap();
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
-    let sio = Sio::new(pac.SIO);
 
     // External high-speed crystal on the pico board is 12Mhz
     let external_xtal_freq_hz = 12_000_000u32;
@@ -45,7 +44,13 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
+    use hal::multicore;
+    let mut mc = multicore::Multicore::new(&mut pac.PSM, &mut pac.PPB, &mut pac.SIO);
+    mc.spawn(1, test);
+
+    //let sio = Sio::new(pac.SIO);
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().integer());
+    /*
 
     let pins = hal::gpio::Pins::new(
         pac.IO_BANK0,
@@ -55,13 +60,57 @@ fn main() -> ! {
     );
 
     let mut led_pin = pins.gpio25.into_push_pull_output();
+    */
 
+    delay.delay_ms(1000);
     loop {
+        info!("A!");
+        /*
+        //led_pin.set_high().unwrap();
+        delay.delay_ms(501);
+        info!("B!");
+        //led_pin.set_low().unwrap();
+        delay.delay_ms(501);
+        */
+    }
+}
+
+fn test() -> ! {
+    let mut pac = unsafe { pac::Peripherals::steal() };
+    let core = unsafe { pac::CorePeripherals::steal() };
+
+    let external_xtal_freq_hz = 12_000_000u32;
+    let mut watchdog = Watchdog::new(pac.WATCHDOG);
+    let clocks = init_clocks_and_plls(
+        external_xtal_freq_hz,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+    let sio = Sio::new(pac.SIO);
+    let pins = hal::gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+    let mut led_pin = pins.gpio25.into_push_pull_output();
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().integer());
+    loop {
+        info!("B!");
+        /*
         info!("on!");
         led_pin.set_high().unwrap();
         delay.delay_ms(500);
         info!("off!");
         led_pin.set_low().unwrap();
         delay.delay_ms(500);
+        */
     }
+    //pac.PSM.frce_off.modify(|_, w| w.proc0().set_bit());
 }
